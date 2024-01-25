fac3e767d07333e02f53de8c06296779c628f8f0
Attila Huszár <attila.huszar@outlook.com>
2024-01-25 23:55:36 +0100

    Add Countdown Timer

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+const config = {
+  endOfLine: 'lf',
+  tabWidth: 2,
+  semi: false,
+  bracketSameLine: true,
+  singleQuote: true,
+}
+
+module.exports = config
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Timer } from './components/Timer'
+
+function App() {
+  return (
+    <div>
+      <Timer />
+    </div>
+  )
+}
+
+export default App
